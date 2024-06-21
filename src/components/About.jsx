60b744873cb93fb478a617602677746b873c91c5
Tharan B <tharanbala33@gmai.com>
2024-06-21 17:38:54 -0500

    First Portfolio Commit

---
+++ src/components/About.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+const container = (delay) => ({
+    hidden: {x: -100, opacity: 0},
+    visible: {
+      x: 0, 
+      opacity: 1, 
+      transition: {duration: 0.5, delay: delay},
+    },
+  });
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#1E1E1E] text-gray-300'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-b-yellow-300'>About</p>
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                <div className='sm:text-right text-4xl font-bold'>
+                    <motion.p variants={container(0)} initial="hidden" animate="visible" className='text-2xl py-4'><span className='text-yellow-300'>Phone number: </span>(414) 243-5462</motion.p>
+                    <motion.p variants={container(0)} initial="hidden" animate="visible" className='text-2xl py-4'><span className='text-yellow-300'>Email: </span> tharanbala33@gmail.com</motion.p>
+                    <motion.p variants={container(0)} initial="hidden" animate="visible" className='text-2xl py-4'><span className='text-yellow-300'>Address: </span> Milwaukee, WI</motion.p>
+                </div>
+                <div>
+                    <motion.p initial={{x:100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5, delay:0}} className='text-2xl'>With over three years of experience in Full Stack Development, I have utilized Python (Flask, Django), React.js, Typescript, TailwindCSS, Data Science and Cloud
+                technologies to successfully execute numerous projects, delivering results that have enhanced business performance. I am
+                recognized as a proficient decision-maker, a reliable team leader, and possess strong communication skills</motion.p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Work.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { EXPERIENCE } from '../constants'
+import {motion} from 'framer-motion'
+
+const container = (delay) => ({
+    hidden: {x: -100, opacity: 0},
+    visible: {
+      x: 0, 
+      opacity: 1, 
+      transition: {duration: 0.5, delay: delay},
+    },
+  });
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full bg-[#1E1E1E] text-gray-300'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-yellow-300'>Experiences</p>
+                <p className='py-6'>Check out some of my recent work and experiences</p>
+            </div>
+            <div>
+                {EXPERIENCE.map((experience, index) => (
+                    <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
+                        <div className='w-full lg:w-1/4'>
+                            <motion.p variants={container(0)} initial="hidden" animate="visible" className='mb-2 text-sm text-neutral-400'>{experience.year}</motion.p>
+                        </div>
+                        <div className='w-full max-w-xl lg:w-3/4'>
+                            <motion.h6 initial={{x:100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5, delay:0}} className='mb-2 font-semibold'>{experience.role} - <span className='text-sm text-purple-100'>{experience.company}</span></motion.h6>
+                            <motion.p initial={{x:100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5, delay:0}} className='mb-4 text-neutral-400'>{experience.description}</motion.p>
+                            {experience.technologies.map((tech, index) => 
+                                <motion.span initial={{x:100, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:0.5, delay:0}} key={index} className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>{tech}</motion.span>
+                            )}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Work
